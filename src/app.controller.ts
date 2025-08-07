3400b88c1a74a1ed7b8009062cff0fa411fcd439
andrew99 <earlbosworth159@gmail.com>
2025-08-07 07:16:51 +0700

    add new api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('version')
+  getVersion(): { version: string } {
+    return { version: '0.0.1' };
+  }
 }
