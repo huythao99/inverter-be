a502f5fd93f1f8ed5697bc25f98bdde7fc0b9d40
andrew99 <earlbosworth159@gmail.com>
2025-09-24 09:27:13 +0700

    save data to record

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,7 +13,7 @@ export class AppController {
 
   @Get('version')
   getVersion(): { version: string } {
-    return { version: '0.2.9' };
+    return { version: '0.2.10' };
   }
 
   @Get('app-ads.txt')
