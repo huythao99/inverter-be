2a3799dd221097214c51a502afed0055d4b6368f
thao.ngo_dev <thao.ngo@dnse.com.vn>
2026-09-15 14:30:38 +0700

    prevent update firmware

--- cms-admin/src/pages/DeviceDetail.tsx
+++ cms-admin/src/pages/DeviceDetail.tsx
@@ -274,7 +274,7 @@ const DeviceDetail: React.FC = () => {
 
     setIsUpdatingFirmware(true);
     try {
-      await triggerFirmwareUpdate(data.device._id, '1.0.10');
+      await triggerFirmwareUpdate(data.device._id, '1.0.8');
       alert(
         'Firmware update triggered successfully! The device will begin updating.',
       );
